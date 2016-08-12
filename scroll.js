591bd97b650e3ead8459a49e17a64367962792c3
Ayanize Co <info@ayanize.com>
2016-08-12 15:26:15 +0530

    Create scroll.js

---
+++ scroll.js
@@ -0,0 +1,31 @@
+<script type="text/javascript">
+(function($) {
+$(document).ready(function() {
+    var navChildren = $("#top-menu li").children();
+    var aArray = [];
+    for (var i = 0; i < navChildren.length; i++) {
+        var aChild = navChildren[i];
+        var ahref = $(aChild).attr('href');
+        aArray.push(ahref);
+    }
+    $(window).scroll(function() {
+        var windowPos = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        var docHeight = $(document).height();
+        for (var i = 0; i < aArray.length; i++) {
+            var theID = aArray[i];
+            var secPosition = $(theID).offset().top;
+            secPosition = secPosition - 135;
+            var divHeight = $(theID).height();
+            divHeight = divHeight + 90;
+            if (windowPos >= secPosition && windowPos < (secPosition + divHeight)) {
+                $("a[href='" + theID + "']").parent().addClass("active");
+            } else {
+                $("a[href='" + theID + "']").parent().removeClass("active");
+            }
+        }
+    });
+
+});
+})(jQuery);
+</script>
